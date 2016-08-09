26042041882ed8ccae1c5b02432857166f8df8db
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-08-09 18:39:17 +0200

    remove the single-assignment exception from consistency step

--- src/test/scala/dropInconsistentAssignments.scala
+++ src/test/scala/dropInconsistentAssignments.scala
@@ -58,14 +58,7 @@
   This step actually consists in two separate steps:
 
   1. We run MG7 with input the output from the drop redundant assignments step, and as reference database the same but the sequence we are using as query.
-  2. For each sequence we check the relation of its assignments with the corresponding LCA that we've got from MG7. If some assignment is too far away from the LCA in the taxonomic tree, it is discarded.
-
-    Right now, if a sequence has only one (single) assignment, it is not tested with the described filter, because
-
-    + it may represent a rare organism that doesn't necessarily have relations with its taxonomic neighbors
-    + it can be an organism that was originally misclassified and put in the taxonomy far away from its real relatives (that could be discovered later, for example)
-
-    After this step BLAST database is generated again.
+  2. For each sequence we check the relation of its assignments with the corresponding LCA that we've got from MG7. If some assignment is too far away from the LCA in the taxonomic tree, it is discarded. After this step the BLAST database is generated again.
 
   Almost all `99.8%` of the sequences from the drop redundant assignments step pass  this filter, because it's mostly about filtering out *wrong* assignments and there are not many sequences that get all assignments discarded.
 */
@@ -104,31 +97,24 @@ case object dropInconsistentAssignments extends FilterDataFrom(dropRedundantAssi
 
         val (id, taxas) = idTaxasFromRow(row)
 
-        /* If there's only one assignment we don't touch it */
-        // NOTE: see https://github.com/ohnosequences/db.rna16s/pull/32#discussion_r71972097 for the reasons
-        if (taxas.length == 1)
-          accept(id, taxas, fasta)
-        else {
+        id2mg7lca.get(id)
+          .flatMap(taxonomyGraph.getTaxon)
+          .flatMap(_.parent)
+          /*
+            Note that the previous filters guarantee that the mg7 LCA IDs *are* in the NCBI taxonomy graph; thus this option will be None if either
 
-          id2mg7lca.get(id)
-            .flatMap(taxonomyGraph.getTaxon)
-            .flatMap(_.parent)
-            /*
-              Note that the previous filters guarantee that the mg7 LCA IDs *are* in the NCBI taxonomy graph; thus this option will be None if either
+            1. this id is not in the MG7 lca output, so that this query sequence has no hits with anything but itself. In that case we need to consider its assignment correct, thus the base case of the fold.
+            2. If the lca has no parent = is the root node, then we can already accept it: it will be in the lineage of every node
+          */
+          .fold( accept(id, taxas, fasta) ) {
+            lcaParent => {
+              /* Here we discard those taxa whose lineage does **not** contain the *parent* of the lca assignment. */
+              val (acceptedTaxas, rejectedTaxas) =
+                taxas.partition { taxa => (taxonomyGraph getTaxon taxa).fold(false)( lcaParent isInLineageOf _ ) }
 
-              1. this id is not in the MG7 lca output, so that this query sequence has no hits with anything but itself. In that case we need to consider its assignment correct, thus the base case of the fold.
-              2. If the lca has no parent = is the root node, then we can already accept it: it will be in the lineage of every node
-            */
-            .fold( accept(id, taxas, fasta) ) {
-              lcaParent => {
-                /* Here we discard those taxa whose lineage does **not** contain the *parent* of the lca assignment. */
-                val (acceptedTaxas, rejectedTaxas) =
-                  taxas.partition { taxa => (taxonomyGraph getTaxon taxa).fold(false)( lcaParent isInLineageOf _ ) }
-
-                writeOutput(id, acceptedTaxas, rejectedTaxas, fasta)
-              }
+              writeOutput(id, acceptedTaxas, rejectedTaxas, fasta)
             }
-        }
+          }
       }
     }
   }
