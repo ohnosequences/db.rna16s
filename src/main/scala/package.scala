340c62cff41d0dce1d67ebf2edb5ecc21c3eadc4
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2018-02-12 10:58:23 +0100

    change db path and version

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -4,26 +4,24 @@ import ohnosequences.awstools.s3._
 
 package object rna16s {
 
-  type +[A, B] = Either[A, B]
+  type +[A, B] =
+    Either[A, B]
 
-  implicit class PredicateOps[X](val p: X => Boolean) {
+  implicit final class PredicateOps[X](val p: X => Boolean) {
 
     def &&(other: X => Boolean): X => Boolean =
       x => p(x) && other(x)
   }
 
-  val dbName: String =
-    "ohnosequences.db.rna16s"
+  val version: String =
+    "7.0"
 
   val asPath: String => String =
     _.replace('.', '/')
 
-  // val metadata: ohnosequences.statika.AnyArtifactMetadata =
-  //   ohnosequences.db.generated.metadata.rna16s
-
   val s3prefix: S3Folder =
     s3"resources.ohnosequences.com" /
-      asPath(dbName) /
-      "argh" /
-  // metadata.version /
+      "db" /
+      "rna16s" /
+      version /
 }
