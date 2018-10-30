9c7ac02be1fbe1aa293ba1d13377ca30a7b13987
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-10-30 17:10:23 +0100

    Move release-related code to main package

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -4,4 +4,10 @@ package object rna16s {
 
   type RNACentralVersion = ohnosequences.db.rnacentral.Version
   type +[A, B]           = Either[A, B]
+
+  implicit final class PredicateOps[X](val p: X => Boolean) extends AnyVal {
+
+    def &&(other: X => Boolean): X => Boolean =
+      x => p(x) && other(x)
+  }
 }
